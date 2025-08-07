2471dd6ddc0afe8ecfef94151ef4e27b7f62a1ef
Louis Ouellet <louis_ouellet@hotmail.com>
2025-08-07 16:06:33 -0400

    General: Small patches to complete the feed component

--- library.js
+++ library.js
@@ -30,6 +30,9 @@ builder.add('components','feed', class extends builder.ComponentClass {
         if(this._properties.class.component){
             this._component.addClass(this._properties.class.component);
         }
+
+        // Create Feed Content
+        this._component.content = $(document.createElement('div')).addClass('feed-content').appendTo(this._component);
     }
 
     control(label, icon = 'circle', callback = null){
@@ -106,27 +109,30 @@ builder.add('components','feed', class extends builder.ComponentClass {
         var post = $(document.createElement('div')).attr({
             'id': postId,
             'class': 'post',
-        }).appendTo(this._component);
+        }).prependTo(this._component.content);
 
         // Set Post ID
         post.id = post.attr('id');
 
+        // Set Post Data
+        post.data = data;
+
         // Create Title Block
         post.header = $(document.createElement('div')).addClass('title').appendTo(post);
-        post.header.title = $(document.createElement('h2')).addClass('title').text(data.title ?? '').appendTo(post.header);
+        post.header.title = $(document.createElement('h2')).addClass('title').text(data.title ?? (data.subject ?? '')).appendTo(post.header);
 
         // Create User Block
         post.owner = $(document.createElement('div')).addClass('owner').appendTo(post);
         post.owner.avatar = $(document.createElement('img')).attr({
             'class': 'avatar',
             'alt': 'Avatar',
-            'src': data.avatar ?? '/avatar?username=' + (data.owner ?? 'anonymous')
+            'src': data.avatar ?? '/avatar?username=' + (data.owner.username ?? (data.owner ?? 'anonymous'))
         }).appendTo(post.owner);
         post.owner.meta = $(document.createElement('div')).addClass('meta').appendTo(post.owner);
         post.owner.meta.username = $(document.createElement('a')).attr({
             'class': 'username',
-            'href': data.profileUrl ?? '/profile'
-        }).text(data.owner ?? 'anonymous').appendTo(post.owner.meta);
+            'href': data.profileUrl ?? '#'
+        }).text(data.owner.username ?? (data.owner ?? 'anonymous')).appendTo(post.owner.meta);
         post.owner.meta.metadata = $(document.createElement('div')).addClass('metadata').appendTo(post.owner.meta);
         post.owner.meta.metadata.icon = $(document.createElement('i')).addClass('bi bi-clock me-1').appendTo(post.owner.meta.metadata);
         post.owner.meta.metadata.timeago = $(document.createElement('time')).attr({
@@ -204,36 +210,50 @@ builder.add('components','feed', class extends builder.ComponentClass {
         });
 
         // Create an event listener on the post content to toggle the visibility of the more block.
-        post.content.on('DOMSubtreeModified', function() {
-            if(post.content[0].scrollHeight > 250){
-                post.more.show();
-            } else {
-                post.more.hide();
-            }
+        const MAX_HEIGHT = 250;
+        function check() {
+            // Use rAF to batch DOM reads/writes
+            requestAnimationFrame(() => {
+                const needsMore = post.content[0].scrollHeight > MAX_HEIGHT;
+                post.more.toggle(needsMore);
+            });
+        }
+
+        // Observe DOM mutations inside the content (text/HTML injected, nodes added, etc.)
+        post._mutationObserver = new MutationObserver(check);
+        post._mutationObserver.observe(post.content[0], {
+            subtree: true,
+            childList: true,
+            characterData: true
         });
+
+        // Observe size changes (images loading, fonts, CSS changes)
+        post._resizeObserver = new ResizeObserver(() => check());
+        post._resizeObserver.observe(post.content[0]);
 
         // Create Controls Block
         post.controls = $(document.createElement('p')).addClass('controls').appendTo(post);
         for(const [label, control] of Object.entries(this._controls)){
-            post.controls[label] = $(document.createElement('button')).attr({
-                'class': 'btn btn-sm btn-link',
+            const controlId = label.toLowerCase().replace(/\s+/g, '-');
+            post.controls[controlId] = $(document.createElement('button')).attr({
+                'class': 'btn btn-sm btn-link action-' + controlId,
                 'type': 'button',
             }).text(self._builder.Locale.get(label)).prepend('<i class="bi me-1 bi-'+control.icon+'"></i>').appendTo(post.controls);
             if(typeof control.callback === 'function'){
-                post.controls[label].on('click', function(e){
+                post.controls[controlId].on('click', function(e){
                     e.preventDefault();
-                    control.callback.call(self, data, post);
+                    control.callback(post);
                 });
             }
         }
 
         // Trigger Callback
         if(typeof callback === 'function'){
-            callback(this, data, post);
+            post = callback(post) ?? post;
         }
 
-        // Trigger the event to check the content height
-        post.content.trigger('DOMSubtreeModified');
+        // Initial check
+        check();
 
         // Set Search
         this._builder.Search.set(post)
